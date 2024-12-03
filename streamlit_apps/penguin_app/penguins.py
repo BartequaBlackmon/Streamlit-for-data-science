aeafab40fe8b8ecc2f5815dab6d6b1ef3bf9d55a
Bartequa Blackmon <bartequa@gmail.com>
2024-12-02 21:20:24 -0500

    adding a cache decorator

--- streamlit_apps/penguin_app/penguins.py
+++ streamlit_apps/penguin_app/penguins.py
@@ -2,20 +2,38 @@ import streamlit as st
 import pandas as pd 
 import altair as alt
 import seaborn as sns
+import time
 
 st.title("palmer's Penguins")
+st.markdown('Use this Streamlit app to make your own scatterplot about penguins!')
 # import the data
 penguin_file = st.file_uploader("Select Your Local Penguins CSV (default provided)")
-if penguin_file is not None:
-    penguins_df = pd.read_csv(penguin_file)
-else:
-    penguins_df = pd.read_csv("C:/Users/black/Downloads/penguins.csv")
-st.markdown('Use this Streamlit app to make your own scatterplot about penguins!')
-#selected_species = st.selectbox('What species would you like to visualize?', ['Adelie', 'Gentoo', 'Chinstrap'])
-selected_x_var = st. selectbox('What do you want the x variable to be?', ['bill_length_mm', 'bill_depth_mm', 'flippr_length_mm', 'body_mass_g'])
-selected_y_var = st.selectbox('What about the y?', ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g'])
+@st.cache_data()
+def load_file(penguins_file):
+    time.sleep(3)
+    if penguin_file is not None:
+        df = pd.read_csv(penguin_file)
+    else:
+        df = pd.read_csv("C:/Users/black/Downloads/penguins.csv")
+    return(df)
+penguins_df = load_file(penguin_file)
 
-#penguins_df = penguins_df[penguins_df['species'] == selected_species]
+#selected_species = st.selectbox('What species would you like to visualize?', ['Adelie', 'Gentoo', 'Chinstrap'])
+selected_x_var = st. selectbox('What do you want the x variable to be?', 
+                               ['bill_length_mm', 'bill_depth_mm', 
+'flippr_length_mm', 'body_mass_g'])
+selected_y_var = st.selectbox('What about the y?', 
+                              ['bill_length_mm', 'bill_depth_mm', 
+'flipper_length_mm', 'body_mass_g'])
+
+selected_gender = st.selectbox('What gender do you want to filter for?',
+                               ['all penguins', 'male penguins', 'female penguins'])
+if selected_gender == 'male penguins':
+    penguins_df = penguins_df[penguins_df['sex'] == 'male']
+elif selected_gender == 'female penguins':
+    penguins_df = penguins_df[penguins_df['sex'] == 'female']
+else:
+    pass
 alt_chart = (
     alt.Chart(penguins_df, title ="Scatterplot of Penguins")
     .mark_circle()
